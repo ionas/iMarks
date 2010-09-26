0f8d3f2e0be0dc7f055ccbe86b3986150d801c60
ionas <code@jonas-hartmann.com>
2010-09-26 21:03:49 +0200

    created tags, taggings

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100926184142) do
+ActiveRecord::Schema.define(:version => 20100926190325) do
 
   create_table "bookmarks", :force => true do |t|
     t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "bookmark_id"
+    t.integer  "tag_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
