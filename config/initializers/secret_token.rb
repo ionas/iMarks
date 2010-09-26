231a531175cde0452fc2877cce0f78d02a03e083
ionas <code@jonas-hartmann.com>
2010-09-26 20:40:57 +0200

    init imark

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imark::Application.config.secret_token = '750bde1b12016cde165b4e7fcb0780314608cd7a44c1c71c3b9af2dadf6c05e9106fc90da4c36abfcb2dc031f978a790cc1e866f7aa225f430d9696ac2bcd4d8'
