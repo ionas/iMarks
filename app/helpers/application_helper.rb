8c349b640fed7a8ec4f71e39075ac37fbf95d3dd
ionas <code@jonas-hartmann.com>
2010-09-27 12:41:28 +0200

    css changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def sortable(column, title = nil)
     title ||= column.titelize
     css_class = column == sort_column ? "sort_current sort_#{sort_direction}" : nil
-    direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
+    direction = column == sort_column && sort_direction == 'desc' ? 'asc' : 'desc'
     link_to title, {:sort_by => column, :sort_direction => direction}, {:class => css_class}
   end
 end
